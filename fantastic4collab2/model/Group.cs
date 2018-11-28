82e2343c3be805e5ee1668ac3bf0a8f1cb7db31d
Brendan_Ralston <ralston.brendan@me.com>
2018-11-27 21:18:58 -0700

    Added initial model definitions

---
+++ fantastic4collab2/model/Group.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fantastic4collab2.model
+{
+    public class Group
+    {
+        IList<Item> items;
+
+        public Group(string name)
+        {
+            this.Name = name;
+        }
+
+        string Name
+        {
+            get => Name;
+            set => Name = value;
+        }
+
+        public void AddItem(Item item)
+        {
+            items.Add(item);
+        }
+
+        public void RemoveItem(Item item)
+        {
+            items.Remove(item);
+        }
+    }
+}
---
+++ fantastic4collab2/model/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fantastic4collab2.model
+{
+    public class Item
+    {
+        public Item(string title, string content)
+        {
+            this.Title = title;
+            this.Content = content;
+            this.Locked = false;
+        }
+
+        public string Title
+        {
+            get => Title;
+            set => Title = value;
+        }
+
+        public string Content
+        {
+            get => Content;
+            set => Content = value;
+        }
+
+        public bool Locked
+        {
+            get => Locked;
+            set => Locked = value;
+        }        
+    }
+}
