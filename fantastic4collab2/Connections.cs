c94243f15e2ee53ae7c5016172f166120771f718
Ross.Allen <r.dallen.dev@gmail.com>
2018-12-05 19:42:12 -0700

    Set up signalr tutorial code and verified that it works. Added SignalR dependencies. Moved startup file to project root.

---
+++ fantastic4collab2/Connections.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fantastic4collab2
+{
+    public class Connections
+    {
+        private static List<string> currentConnections = new List<string>();
+
+        public int Count
+        {
+            get
+            {
+                return currentConnections.Count;
+            }
+        }
+
+        public void Add(string connectionId)
+        {
+            currentConnections.Add(connectionId);
+        }
+
+        public List<string> getConnections() {
+            return currentConnections;
+        }
+    }
+}
---
+++ fantastic4collab2/Hubs/AppHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace fantastic4collab2.Hubs
+{
+    public class AppHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Console.WriteLine("Connection ID: " + Context.ConnectionId);
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
