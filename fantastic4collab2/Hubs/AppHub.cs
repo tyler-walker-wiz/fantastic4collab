b5aa21f80c861476f4b53e0b6e753d77e1740886
Ross.Allen <r.dallen.dev@gmail.com>
2018-12-05 21:13:43 -0700

    added method to hub that gets singleton data on connect. Set up client for testing method.

--- fantastic4collab2/Hubs/AppHub.cs
+++ fantastic4collab2/Hubs/AppHub.cs
@@ -4,16 +4,44 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using System.Threading.Tasks;
+using fantastic4collab2.Models;
 
 namespace fantastic4collab2.Hubs
 {
     public class AppHub : Hub
     {
+        private static List<string> currentConnections = new List<string>();
+
         public void Send(string name, string message)
         {
             Console.WriteLine("Connection ID: " + Context.ConnectionId);
             // Call the broadcastMessage method to update clients.
             Clients.All.broadcastMessage(name, message);
+        }
+
+        public override Task OnConnected()
+        {
+            currentConnections.Add(Context.ConnectionId);
+
+            Singleton singletonInstance = Singleton.Instance;
+
+            Clients.All.getItem(singletonInstance.GetItem(1, 1));
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            currentConnections.Remove(Context.ConnectionId);
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public override Task OnReconnected()
+        {
+            if (!currentConnections.Contains(Context.ConnectionId)) {
+                currentConnections.Add(Context.ConnectionId);
+            }
+            return base.OnReconnected();
         }
     }
 }
